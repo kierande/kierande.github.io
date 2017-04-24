f9f590e8596c43e0058eb034fd76a11f943aae37
kierande <design@kierandelaney.co.uk>
2017-04-24 08:49:14 +0100

    Update test.js

--- test.js
+++ test.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         
         //send to formspree
         $.ajax({
-            url:'https://formspree.io/submissions@kierandelaney.co.uk',
+      url: "//formspree.io/submissions@kierandelaney.co.uk",
             method:'POST',
             data:{
                 name:name,
